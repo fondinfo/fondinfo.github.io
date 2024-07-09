807f26fd0c87de2b05b841d308f6c14476178cfe
Fondamenti di Informatica <144357422+fondinfo@users.noreply.github.com>
2024-07-09 12:23:05 +0200

    Update Python-client_01.py

--- web/Python-client_01.py
+++ web/Python-client_01.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""
+@author  Alberto Ferrari - https://albertoferrari.github.io/
+@license This software is free - https://opensource.org/license/mit
+"""
 import requests
 
 response = requests.get("http://localhost:8000/test.html")
