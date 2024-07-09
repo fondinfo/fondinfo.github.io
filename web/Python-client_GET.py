4e1d59ca914ed6dcc7a1bbc842700a4419d02189
Fondamenti di Informatica <144357422+fondinfo@users.noreply.github.com>
2024-07-09 12:23:58 +0200

    Update Python-client_GET.py

--- web/Python-client_GET.py
+++ web/Python-client_GET.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""
+@author  Alberto Ferrari - https://albertoferrari.github.io/
+@license This software is free - https://opensource.org/license/mit
+"""
 import requests
 
 url = 'http://localhost:8000/test.html?name=Turing&born=1912'
